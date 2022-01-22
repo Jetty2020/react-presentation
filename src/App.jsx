959e30513776eeedb33cf5a8ad530074b9741a3b
JeongHoon Park <kmclo@naver.com>
2022-01-23 02:25:20 +0900

    :lipstick: Global style 적용 - reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import { Global } from '@emotion/react';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { routes } from './constants';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import { GlobalStyles } from './styles/globals';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={routes.home} element={<Home />} />
-        <Route path={routes.login} element={<Login />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Global styles={GlobalStyles} />
+      <BrowserRouter>
+        <Routes>
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.login} element={<Login />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
