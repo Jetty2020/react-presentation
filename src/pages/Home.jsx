02b4fff0b6f6808e22ce618e897ea84df8c5b8f8
JeongHoon Park <kmclo@naver.com>
2022-01-23 14:18:21 +0900

    :alembic: Redux toolkit store에서 값 가져오기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PageTitle from '../components/common/PageTitle';
 
 function Home() {
+  const data = useSelector((state) => state.user);
+  console.log('home', data);
   return (
     <div>
       <PageTitle title="Home" />
+      <Link to="/login">로그인</Link>
       <Text>Home</Text>
     </div>
   );
