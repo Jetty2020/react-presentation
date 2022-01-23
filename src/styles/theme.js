f78e26299f4a04dc5790f24223daa9bc8a1f2e9b
JeongHoon Park <kmclo@naver.com>
2022-01-23 12:09:26 +0900

    :lipstick: Global theme 적용

---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    primary: '#ff8a3d',
+    black: '#000000',
+    red: '#f77f77',
+    grey: '#9e9e9e',
+    lightGrey: '#eeeeee',
+    deepGrey: '#757575',
+    darkGrey: '#656e75',
+  },
+  mediaQuery: {
+    mobile: '375px',
+    tablet: '768px',
+    laptop: '1024px',
+    desktop: '1600px',
+  },
+};
