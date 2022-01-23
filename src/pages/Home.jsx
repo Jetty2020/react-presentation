495a7828267a685055e00d8c662ec4cefd8769e7
JeongHoon Park <kmclo@naver.com>
2022-01-23 12:44:00 +0900

    :truck: PageTitle.jsx common dir로 이동

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import PageTitle from '../components/PageTitle';
+import PageTitle from '../components/common/PageTitle';
 
 function Home() {
   return (
