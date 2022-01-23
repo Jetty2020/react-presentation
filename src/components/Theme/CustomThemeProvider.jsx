6ea6433425ec9e07b3a247c7938511a05e4f4fa7
JeongHoon Park <kmclo@naver.com>
2022-01-23 12:55:57 +0900

    :lipstick: Dark mode button UI 구현

--- src/components/Theme/CustomThemeProvider.jsx
+++ src/components/Theme/CustomThemeProvider.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import propTypes from 'prop-types';
+import styled from '@emotion/styled';
 import { Global, ThemeProvider } from '@emotion/react';
 import { mode } from '../../styles/theme';
 import { GlobalStyles } from '../../styles/globals';
@@ -19,9 +20,9 @@ function CustomThemeProvider({ children }) {
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyles(theme)} />
       {children}
-      <button type="button" onClick={handleMode}>
-        다크모드 버튼
-      </button>
+      <DarkModeBtn type="button" onClick={handleMode}>
+        {dark ? '라이트 모드로 보기' : '다크 모드로 보기'}
+      </DarkModeBtn>
     </ThemeProvider>
   );
 }
@@ -34,3 +35,15 @@ CustomThemeProvider.propTypes = {
     propTypes.node,
   ]).isRequired,
 };
+
+const DarkModeBtn = styled.button`
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  height: 40px;
+  padding: 0 25px;
+  border-radius: 20px;
+  background: ${({ theme }) => theme.bg.darkBtn};
+  color: ${({ theme }) => theme.text.darkBtn};
+  font-weight: 600;
+`;
